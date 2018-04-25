e156e64d87f4411f277e94bdad5e9464fc42036d
Ilma Lusija <ilusija1@etf.unsa.ba>
2018-04-25 15:45:01 +0200

    ilma majstor

--- Projekat/EventPlanner/MainPage.xaml.cs
+++ Projekat/EventPlanner/MainPage.xaml.cs
@@ -26,5 +26,10 @@ namespace EventPlanner
         {
             this.InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
